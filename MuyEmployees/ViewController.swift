debc43b930b8b71fdd0fa8152bf86f5f4ffed8b7
DANIELMCKENSI <DANIEL.STEVEN.MURCIA@gmail.com>
2020-08-28 10:20:32 -0500

    Initial Commit

---
+++ MuyEmployees/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MuyEmployees
+//
+//  Created by Daniel Steven Murcia Almanza on 28/08/20.
+//  Copyright © 2020 selvamatic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
